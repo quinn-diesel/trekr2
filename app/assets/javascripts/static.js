2e1be88f451a7473dfd7ba326c5374ead2eb56d1
Quinn Stobbs <q.stobbs@gmail.com>
2017-07-14 10:12:57 +1000

    nav bar 1 up and working

--- app/assets/javascripts/static.js
+++ app/assets/javascripts/static.js
@@ -1,29 +1,29 @@
 
-$(document).ready(function () {
-
-
-    if( $('.static.show').length){
-
-    window.onresize = function(event){
-        resizeDiv();
-    }
-
-    function resizeDiv(){
-        vpw = $(window).width();
-        vph = $(window).height();
-        $('#welcomeMap').css({'width': vpw});
-        $('#welcomeMap').css({'height': vph});
-    }
-
-
-    var mymap = L.map('welcomeMap').setView([-33.804122, 151.246096], 2);
-
-    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-        maxZoom: 18,
-        id: 'mapbox.pirate',
-        accessToken: 'pk.eyJ1IjoicXVpbm5kcmljayIsImEiOiJjajR4Z2s3b3Yxajg4MnFxcGpnd3owMW4zIn0.MggRMIsoIc9X5Iga_MLEPw'
-    }).addTo(mymap);
-
-    }; //if (static show page)
-}); // doc ready
+// $(document).ready(function () {
+//
+//
+//     if( $('.static.show').length){
+//
+//     window.onresize = function(event){
+//         resizeDiv();
+//     }
+//
+//     function resizeDiv(){
+//         vpw = $(window).width();
+//         vph = $(window).height();
+//         $('#welcomeMap').css({'width': vpw});
+//         $('#welcomeMap').css({'height': vph});
+//     }
+//
+//
+//     var mymap = L.map('welcomeMap').setView([-33.804122, 151.246096], 2);
+//
+//     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+//         attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+//         maxZoom: 18,
+//         id: 'mapbox.pirate',
+//         accessToken: 'pk.eyJ1IjoicXVpbm5kcmljayIsImEiOiJjajR4Z2s3b3Yxajg4MnFxcGpnd3owMW4zIn0.MggRMIsoIc9X5Iga_MLEPw'
+//     }).addTo(mymap);
+//
+//     }; //if (static show page)
+// }); // doc ready
